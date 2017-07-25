e239a95e7858d046bb845fab102e9bdc76140627
Vedang <f2015028@goa.bits-pilani.ac.in>
2017-07-25 18:38:56 +0530

    Added plotly functionality

--- PS/Code/classifier.py
+++ PS/Code/classifier.py
@@ -17,12 +17,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from time import time
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
 from fetch_data import fetch_feature_vector, fetch_labels_vector
 
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+import plotly
 import plotly.graph_objs as go
 
 def ask_for_data():
@@ -44,9 +41,6 @@ def main():
 	features_train = fetch_feature_vector()
 	labels_train = fetch_labels_vector()
 
-#	print features_train, '\n', "Length of feature vector: ", len(features_train)
-#	print labels_train, '\n', "Length of labels vector: ", len(labels_train)
-
 	t0 = time()
 	clf.fit(features_train, labels_train)
 	print "Training time: ", time() - t0, " secs"
@@ -63,93 +57,45 @@ def main():
 			X2.append(features_train[i][0])
 			Y2.append(features_train[i][1])
 
-	"""
-	t = np.arange(-20., 20., 1)
-	fig = plt.figure("Scatterplot of data")
-	axes = plt.axis()
-	print axes
+	slope = clf.coef_[0][1]
+	intercept = clf.coef_[0][0]
+	xi = np.arange(-10, 13, 0.02)
+	yline = slope * xi + intercept
 
-	ax0 = fig.add_subplot(111)
-	ax0.plot(X1, Y1, 'bo', label='Earthquakes', ms = 4)
-	ax0.plot(X2, Y2, 'rx', label='Blasting')
-	ax0.plot(t, clf.coef_[0][1] * t + clf.coef_[0][0], linewidth=1.0)
-	ax0.set_xlabel('Complexity')
-	ax0.set_ylabel('log Pe')
-	ax0.legend()
-	plt.show()
-	"""
-	
-	comp = []
-	logpose = []
-	lbl = []
-	of_class = []
+	trace0 = go.Scatter(
+		x = xi,
+		y = yline,
+		name = 'Hyperplane',
+		mode = 'lines',
+		)
 
-	index = 0
-	for i in features_train:
-		comp.append(i[0])
-		logpose.append(i[1])
-
-		if labels_train[index] == 0:
-			of_class.append('Earthquake')
-		elif labels_train[index] == 1:
-			of_class.append('Blasting')
-		index += 1
-
-	raw_data = {'complexity': comp, 'logpoe': logpose, 'label': labels_train, 'class': of_class}
-	df = pd.DataFrame(raw_data, columns=['complexity', 'logpoe', 'label', 'class'])
-	df.to_csv('~/Desktop/PS/Code/featurestore.csv')
-
-	app = dash.Dash()
-
-	df = pd.read_csv('~/Desktop/PS/Code/featurestore.csv')
-
-	app.layout = html.Div([
-	    dcc.Graph(
-	        id='complexity-vs-logPe',
-	        figure={
-	            'data': [
-	                go.Scatter(
-	                    x=df[df['label'] == i]['complexity'],
-	                    y=df[df['label'] == i]['logpoe'],
-	                    text=df[df['label'] == i]['class'],
-	                    mode='markers',
-	                    opacity=0.7,
-	                    marker={
-	                        'size': 15,
-	                        'line': {'width': 0.5, 'color': 'white'}
-	                    },
-	                    name=i
-	                ) for i in df.label.unique()
-	            ],
-	            'layout': go.Layout(
-	                xaxis={'type': 'log', 'title': 'Complexity'},
-	                yaxis={'title': 'log Pe'},
-	                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-	                legend={'x': 0, 'y': 1},
-	                hovermode='closest'
-	            )
-	        }
+	trace1 = go.Scatter(
+	    x = X1,
+	    y = Y1,
+	    name = 'Earthquake',
+	    mode = 'markers',
+	    opacity = 0.7,
+	    marker=dict(
+	        size='16',
+	        color = 'purple',
 	    )
-	])
+	)
 
-	app.run_server()
+	trace2 = go.Scatter(
+		x = X2,
+		y = Y2,
+		name = 'Blasting',
+		mode = 'markers',
+		opacity = 0.7,
+		marker = dict(
+			size = '16',
+			color = 'orange',
+		)
+	)
 
+	data = [trace0, trace1, trace2]
 
-###################################a
-#	x = ask_for_data()	
-	
-#	x = [[1.0010466633, 0.169843732038], [1.58787884493, 0.769931641951], [1.23733655143, 0.206543477241], [-3.01018059594, 0.560479182515], [0.914409898799, -0.35812293039]]
-#	print "Distances of points from decision boundary: ", clf.decision_function(x)
-	""""
-	pred = clf.predict(x)
+	plotly.offline.plot({"data": data, "layout": go.Layout(title='Scatterplot and Decision Boundary')})
 
-	for prediction in pred:
-		if prediction == 0:
-			print "Earthquake"
-		elif prediction == 1:
-			print "Blasting"
-		else:
-			print "Invalid data, check again."
-	"""
 if __name__ == "__main__":
 	main()
